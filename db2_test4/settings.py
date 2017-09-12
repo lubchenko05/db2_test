4fa01a12033b70202fb4294e21ce369918534495
Yurij Lyubchenko <lubchenko05@gmail.com>
2017-09-12 14:49:47 +0300

    set debug = false

--- db2_test4/settings.py
+++ db2_test4/settings.py
@@ -24,7 +24,8 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 SECRET_KEY = "isythjnzzwn5$%3b#a79b!lv=-zq)o1yac__oov+m^4ssm&%85"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
