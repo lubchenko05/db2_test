f6feeb704b7a2c44c03dfa0b0c0bd09359831d05
Yurij Lyubchenko <lubchenko05@gmail.com>
2017-09-12 14:44:38 +0300

    fix bug

--- db2_test4/settings.py
+++ db2_test4/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'db2_test4.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
